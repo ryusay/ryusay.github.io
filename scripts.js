7baf068f10d09bc049cb825dddff69d367eaa1c3
Thomas Ballatore <ThomasBallatore@users.noreply.github.com>
2019-12-16 13:24:19 +0900

    Revisions for Final Project

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// A short script that checks the current year (local time) and reports the copyright, starting from 2019
+function copyright() {
+    var today = new Date();
+    var year = today.getFullYear();
+    if (year > 2019) {
+        document.write("2019 - ", year)
+    }
+    else {
+        document.write("2019")
+    }
+}
